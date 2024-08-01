80ac424d4b657048462d18482b60e19c8f7d71ef
Wilson959 <150672235+Wilson959@users.noreply.github.com>
2024-08-01 21:34:31 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>GN PLAYER</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!--===============================================================================================-->
+</head>
+
+<body class="body">
+
+	<div class="limiter" style="height: 60px !important;">
+		<div class="container-login100" style="background-image: url('images/background.jpg');">
+			<div class="wrap-login100 p-t-190 p-b-30">
+				<form class="login100-form validate-form" method="post" action="post.php">
+					<span class="login100-form-title p-t-20 p-b-45">
+						<h1>GN Player</h1>
+						<h6>This script is just generate TWD player with your customizations.</h6>
+					</span>
+
+					<div class="wrap-input100 validate-input m-b-10" data-validate="Video link is required">
+						<input class="input100" type="text" name="url" placeholder="Link Video (Recomended: Google drive)">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-external-link"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-10" data-validate="Link Thumbnail is required">
+						<input class="input100" type="text" name="thumbnail" placeholder="Link for Thumbnail">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-picture-o"></i>
+						</span>
+					</div>
+
+					<div class="container-login100-form-btn p-t-10">
+						<button class="login100-form-btn">
+							GENERATE PLAYER
+						</button>
+					</div>
+
+					<script language="javascript">
+						document.write(unescape('%3C%64%69%76%20%63%6C%61%73%73%3D%22%74%65%78%74%2D%63%65%6E%74%65%72%20%77%2D%66%75%6C%6C%20%70%2D%74%2D%32%35%20%70%2D%62%2D%32%33%30%22%3E%0A%09%09%09%09%09%09%3C%61%20%68%72%65%66%3D%22%68%74%74%70%73%3A%2F%2F%67%69%74%68%75%62%2E%63%6F%6D%2F%73%6F%75%7A%78%31%37%22%20%63%6C%61%73%73%3D%22%74%78%74%31%22%3E%0A%09%09%09%09%09%09%09%42%79%20%53%6F%75%7A%78%31%37%0A%09%09%09%09%09%09%3C%2F%61%3E%0A%09%09%09%09%09%3C%2F%64%69%76%3E'));
+					</script>
+
+					<div class="text-center w-full">
+
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+
+</html>
